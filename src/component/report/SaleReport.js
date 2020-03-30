19b66cdd5e4a5f3e558ea58f8cfd4e18f2906f3b
Hien Hoang <hienhoang2702@gmail.com>
2020-03-30 21:10:05 +0700

    fixed bug: data table in sale reports view
    Signed-off-by: Hien Hoang <hienhoang2702@gmail.com>

--- src/component/report/SaleReport.js
+++ src/component/report/SaleReport.js
@@ -18,6 +18,7 @@ export function SaleReportByProduct() {
     <SaleReport
       filterLabel={'Chọn sản phẩm: '}
       filterId={'productId'}
+      filterName={'productName'}
       selectDataRequest={async () =>
         (await authPost(dispatch, token, '/get-list-product', {}).then(r => r.json()))['products']}
       tableTitle={'Báo cáo doanh số theo sản phẩm'}
@@ -33,6 +34,7 @@ export function SaleReportByPartyCustomer() {
     <SaleReport
       filterLabel={'Chọn khách hàng: '}
       filterId={'partyCustomerId'}
+      filterName={'customerName'}
       selectDataRequest={async () => await authGet(dispatch, token, '/get-list-party-customers')}
       tableTitle={'Báo cáo doanh số theo khách hàng'}
     />
@@ -43,11 +45,11 @@ function SaleReport(props) {
   const dispatch = useDispatch();
   const token = useSelector(state => state.auth.token);
 
-  const {filterLabel, filterId, selectDataRequest, tableTitle} = props;
+  const {filterLabel, filterId, filterName, selectDataRequest, tableTitle} = props;
 
   const columns = [
-    {title: "Ngày", field: "orderDate",},
-    {title: "Doanh thu (VND)", field: "totalGrand"},
+    {title: "Ngày", field: "date",},
+    {title: "Doanh thu (VND)", field: "price"},
   ];
 
   const [dataTable, setDataTable] = useState();
@@ -62,14 +64,14 @@ function SaleReport(props) {
     let selectDataResponse = await selectDataRequest(); // get all product/customer....
     setSelectData(selectDataResponse);
     setSelected(selectDataResponse[0][filterId]);
-    await onChangeSelected(selectDataResponse[0][filterId]);
+    await onChangeSelected(fromDate, thruDate, selectDataResponse[0][filterId]);
   }, []);
 
-  async function onChangeSelected(newValue) {
+  async function onChangeSelected(fromDate, thruDate, selectedData) {
     let dataTableResponse = await authPost(dispatch, token, '/get-sale-reports', {
-      fromDate: toFormattedDateTime(fromDate), thruDate: toFormattedDateTime(thruDate), [filterId]: newValue // productId: ... || partyCustomerId: ...
+      fromDate: toFormattedDateTime(fromDate), thruDate: toFormattedDateTime(thruDate), [filterId]: selectedData // productId: ... || partyCustomerId: ...
     }).then(r => r.json());
-    setDataTable(dataTableResponse);
+    setDataTable(dataTableResponse['datePrices']);
   }
 
   return <div>
@@ -83,7 +85,10 @@ function SaleReport(props) {
             margin="normal"
             label="Từ ngày: "
             value={fromDate}
-            onChange={setFromDate}
+            onChange={date => {
+              setFromDate(date);
+              onChangeSelected(date, thruDate, selected).then(r => r);
+            }}
             KeyboardButtonProps={{
               'aria-label': 'change date',
             }}
@@ -95,7 +100,10 @@ function SaleReport(props) {
             margin="normal"
             label="Đến ngày: "
             value={thruDate}
-            onChange={setThruDate}
+            onChange={date => {
+              setThruDate(date);
+              onChangeSelected(fromDate, date, selected).then(r => r);
+            }}
             KeyboardButtonProps={{
               'aria-label': 'change date',
             }}
@@ -105,9 +113,10 @@ function SaleReport(props) {
         {filterLabel + ': '}
         <Select value={selected} onChange={event => {
           setSelected(event.target.value);
-          onChangeSelected(event.target.value).then(r => r);
+          onChangeSelected(fromDate, thruDate, event.target.value).then(r => r);
         }}>
-          {selectData.map(value => <MenuItem value={value[filterId]}> {value[filterId]} </MenuItem>)}
+          {selectData.map(value => <MenuItem value={value[filterId]}>
+            {value[filterName] + ' (' + value[filterId] + ')'} </MenuItem>)}
         </Select>
       </Grid>
       <Grid item xs={4}
