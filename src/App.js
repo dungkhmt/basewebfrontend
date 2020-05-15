503697730be803bd2c71b921578682e0e41f72b6
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-05-15 21:48:33 +0700

    Add SnackbarProvider component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
+import { SnackbarProvider } from "notistack";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Routes isAuthenticated={this.props.isAuthenticated} />
-      </Router>
+      <SnackbarProvider maxSnack={3}>
+        <Router>
+          <Routes isAuthenticated={this.props.isAuthenticated} />
+        </Router>
+      </SnackbarProvider>
     );
   }
 }
