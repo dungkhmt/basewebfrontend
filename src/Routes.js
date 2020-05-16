56df4a3da05bae6167b4522dcb8666e503958175
Hien Hoang <hienhoang2702@gmail.com>
2020-05-16 18:30:26 +0700

    add ProductPriceSupplier list/set view
    Signed-off-by: Hien Hoang <hienhoang2702@gmail.com>

--- src/Routes.js
+++ src/Routes.js
@@ -23,9 +23,9 @@ import ShipmentItemList from "./component/shipment/shipment/ShipmentItemList";
 import VehicleList from "./component/shipment/vehicle/VehicleList";
 import DeliveryTripCreate from "./component/shipment/deliveryplan/deliverytrip/DeliveryTripCreate";
 import DeliveryTripDetailList
-  from "./component/shipment/deliveryplan/deliverytrip/deliverytripdetail/DeliveryTripDetailList";
+    from "./component/shipment/deliveryplan/deliverytrip/deliverytripdetail/DeliveryTripDetailList";
 import DeliveryTripDetailCreate
-  from "./component/shipment/deliveryplan/deliverytrip/deliverytripdetail/DeliveryTripDetailCreate";
+    from "./component/shipment/deliveryplan/deliverytrip/deliverytripdetail/DeliveryTripDetailCreate";
 import ShipmentItemDeliveryPlanList from "./component/shipment/deliveryplan/shipmentitem/ShipmentItemDeliveryPlanList";
 import VehicleDeliveryPlanList from "./component/shipment/deliveryplan/vehicle/VehicleDeliveryPlanList";
 import ShipmentItemCreate from "./component/shipment/shipment/ShipmentItemCreate";
@@ -83,41 +83,41 @@ import PortCreate from "./component/tmscontainer/port/PortCreate";
 import PortList from "./component/tmscontainer/port/PortList";
 import PortGoogleMap from "./component/tmscontainer/port/PortGoogleMap";
 import ShipmentItemDeliveryTripDetailList
-  from "./component/shipment/deliveryplan/shipmentitem/ShipmentItemDeliveryTripDetailList";
+    from "./component/shipment/deliveryplan/shipmentitem/ShipmentItemDeliveryTripDetailList";
 import NotScheduledShipmentItem from "./component/shipment/deliveryplan/deliverytrip/NotScheduledShipmentItem";
 import ProductDetail from "./component/product/detailproduct";
 import {
-  TransportReportByDriver,
-  TransportReportByFacility,
-  TransportReportByPartyCustomer,
+    TransportReportByDriver,
+    TransportReportByFacility,
+    TransportReportByPartyCustomer,
 } from "./component/report/TransportReport";
 import SolverConfigParam from "./component/shipment/solver/SolverConfigParam";
 import CreateRequestTransportContainerToWarehouse
-  from "./component/tmscontainer/requestimportfull/CreateRequestTransportContainerToWarehouse";
+    from "./component/tmscontainer/requestimportfull/CreateRequestTransportContainerToWarehouse";
 import ListRequestTransportContainerToWareHouse
-  from "./component/tmscontainer/requestimportfull/ListRequestTransportContainerToWareHouse";
+    from "./component/tmscontainer/requestimportfull/ListRequestTransportContainerToWareHouse";
 import VehicleDetail from "./component/shipment/vehicle/VehicleDetail";
 import VehicleNotInDeliveryTrips from "./component/shipment/deliveryplan/vehicle/VehicleNotInDeliveryTrips";
 import DriverDetail from "./component/shipment/driver/DriverDetail";
 import CreateRequestTransportContainerEmpty
-  from "./component/tmscontainer/requestimportempty/CreateRequestTransportContainerEmpty";
+    from "./component/tmscontainer/requestimportempty/CreateRequestTransportContainerEmpty";
 import ListRequestTransportContainerEmpty
-  from "./component/tmscontainer/requestimportempty/ListRequestTransportContainerEmpty";
+    from "./component/tmscontainer/requestimportempty/ListRequestTransportContainerEmpty";
 import ListRequestTransportContainerEmptyExport
-  from "./component/tmscontainer/requestexportempty/ListRequestTransportContainerEmptyExport";
+    from "./component/tmscontainer/requestexportempty/ListRequestTransportContainerEmptyExport";
 import CreateRequestTransportContainerEmptyExport
-  from "./component/tmscontainer/requestexportempty/CreateRequestTransportContainerEmptyExport";
+    from "./component/tmscontainer/requestexportempty/CreateRequestTransportContainerEmptyExport";
 import CreateRequestTransportFullExport
-  from "./component/tmscontainer/requestexportfull/CreateRequestTransportFullExport";
+    from "./component/tmscontainer/requestexportfull/CreateRequestTransportFullExport";
 import ListRequestTransportFullExport from "./component/tmscontainer/requestexportfull/ListRequestTransportFullExport";
 import RetailOutletDetail from "./component/retailoutlet/RetailOutletDetail";
 import {
-  DistributorUnpaidInvoiceDetail,
-  DistributorUnpaidInvoiceList,
-  Invoice,
-  InvoiceDetail,
-  Payment,
-  PaymentApplication,
+    DistributorUnpaidInvoiceDetail,
+    DistributorUnpaidInvoiceList,
+    Invoice,
+    InvoiceDetail,
+    Payment,
+    PaymentApplication,
 } from "./component/accounting/InvoiceDataTable";
 import {PaymentCreate} from "./component/accounting/PaymentCreate";
 import PaymentApplicationCreate from "./component/accounting/PaymentApplicationCreate";
@@ -131,6 +131,7 @@ import SalesRoutesPlanningPeriod from "./component/salesroutes/SalesRoutesPlanni
 import SupplierList from "./component/supplier/SupplierList";
 import SupplierCreate from "./component/supplier/SupplierCreate";
 import QuickInvoicePayment from "./component/accounting/QuickInvoicePayment";
+import ProductPriceSupplier from "./component/supplier/ProductPriceSupplier";
 
 function Routes(props) {
   // props nay tu parent transfer vao
@@ -979,14 +980,23 @@ function Routes(props) {
         path="/supplier/list" // props
       />
 
-      <PrivateRouteWithLayout
-        component={SupplierCreate} //props
-        layout={Layout} //props
-        isAuthenticated={props.isAuthenticated} // props
-        //isAuthenticated={true}
-        //exact                                   // props
-        path="/supplier/create" // props
-      />
+        <PrivateRouteWithLayout
+          component={SupplierCreate} //props
+          layout={Layout} //props
+          isAuthenticated={props.isAuthenticated} // props
+          //isAuthenticated={true}
+          //exact                                   // props
+          path="/supplier/create" // props
+        />
+
+        <PrivateRouteWithLayout
+          component={ProductPriceSupplier} //props
+          layout={Layout} //props
+          isAuthenticated={props.isAuthenticated} // props
+          //isAuthenticated={true}
+          //exact                                   // props
+          path="/product-price-supplier/list" // props
+        />
 
     </Switch>
   );
