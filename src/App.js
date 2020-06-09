b45c0f91d95b4bfeb588a95da8503888ea7d2b49
tuandat95cbn <tuandat95cbn@gmail.com>
2020-06-09 07:51:32 +0700

    fix bug error handling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {BrowserRouter as Router} from "react-router-dom";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Routes from "./Routes";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Slide, Zoom, Flip, Bounce } from 'react-toastify';
 
 class App extends Component {
   render() {
     return (
-        <Router>
-          <Routes isAuthenticated={this.props.isAuthenticated} />
-            <ToastContainer
-              position="bottom-left"
-              transition={Slide}
-              autoClose={2000}
-              hideProgressBar={true}
-              newestOnTop
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-            />
-        </Router>
+      <Router>
+        <Routes isAuthenticated={this.props.isAuthenticated} error={this.props.error} />
+        <ToastContainer
+          position="bottom-left"
+          transition={Slide}
+          autoClose={2000}
+          hideProgressBar={true}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </Router>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = (dispatch) => ({});
 export default connect(mapStateToProps, mapDispatchToProps)(App);
