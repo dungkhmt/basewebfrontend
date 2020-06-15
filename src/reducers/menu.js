5a4aa0edeb698f90dcdd4f92c11f60d63e11156a
tuandat95cbn <tuandat95cbn@gmail.com>
2020-06-15 18:09:31 +0700

    add selected menu

--- src/reducers/menu.js
+++ src/reducers/menu.js
@@ -1,22 +1,36 @@
-import {MENU_REQUEST_FAILURE, MENU_REQUEST_SUCCESS, MENU_REQUESTING} from "../action/Menu";
+import {
+  MENU_REQUEST_FAILURE,
+  MENU_REQUEST_SUCCESS,
+  MENU_REQUESTING,
+  SELECTED_FUNCTION_UPDATE,
+} from "../action/Menu";
 
 const menu = (
-  state = {menu: new Set(), isRequesting: false, isMenuGot: false},
+  state = {
+    menu: new Set(),
+    isRequesting: false,
+    isMenuGot: false,
+    selectedFunction: null,
+  },
   action
 ) => {
   switch (action.type) {
     case MENU_REQUESTING:
-      return Object.assign({}, state, {isRequesting: true});
+      return Object.assign({}, state, { isRequesting: true });
     case MENU_REQUEST_SUCCESS:
       return Object.assign({}, state, {
         menu: new Set(action.menu),
         isMenuGot: true,
-        isRequesting: false
+        isRequesting: false,
+      });
+    case SELECTED_FUNCTION_UPDATE:
+      return Object.assign({}, state, {
+        selectedFunction: action.selectedFunction,
       });
     case MENU_REQUEST_FAILURE:
       return Object.assign({}, state, {
         isRequesting: false,
-        isMenuGot: false
+        isMenuGot: false,
       });
 
     default:
