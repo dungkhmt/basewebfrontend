2a40234c292641a216b816596b9f257095af0daf
tuandat95cbn <tuandat95cbn@gmail.com>
2020-07-03 11:21:43 +0700

    refactor menu config

---
+++ src/config/menuconfig/customer.js
@@ -0,0 +1,27 @@
+import { MENU_LIST } from "../menuconfig";
+
+export const customer = {
+  id: "MENU_CUSTOMER",
+  path: "",
+  isPublic: false,
+  icon: "PeopleOutlineIcon",
+  text: "QL Khách hàng",
+  child: [
+    {
+      id: "MENU_CUSTOMER_CREATE",
+      path: "/customer/create",
+      isPublic: false,
+      icon: "StarBorder",
+      text: "Tạo mới KH",
+      child: [],
+    },
+    {
+      id: "MENU_CUSTOMER_VIEW",
+      path: "/customer/list",
+      isPublic: false,
+      icon: "StarBorder",
+      text: "Danh sách KH",
+      child: [],
+    },
+  ],
+};
---
+++ src/config/menuconfig/department.js
@@ -0,0 +1,17 @@
+export const department = {
+  id: "MENU_DEPARTMENT",
+  path: "",
+  isPublic: false,
+  icon: "BlurOnIcon",
+  text: "Quản lý phòng ban",
+  child: [
+    {
+      id: "MENU_DEPARTMENT_LIST",
+      path: "/departments/list",
+      isPublic: true,
+      icon: "StarBorder",
+      text: "DS phòng ban",
+      child: [],
+    },
+  ],
+};
