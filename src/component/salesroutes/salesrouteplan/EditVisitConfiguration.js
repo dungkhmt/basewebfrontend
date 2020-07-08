6efc192a8c2c73a5f13fb4e5306c7022b45fb0c7
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-07-08 18:22:01 +0700

    big upgrade

--- src/component/salesroutes/salesrouteplan/EditVisitConfiguration.js
+++ src/component/salesroutes/salesrouteplan/EditVisitConfiguration.js
@@ -6,21 +6,24 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { authPost, authGet, authPut } from "../../../api";
+import { axiosGet, axiosPut } from "../../../api";
 import { useDispatch, useSelector } from "react-redux";
 import { Save, Cancel } from "@material-ui/icons";
 import { Controller, useForm } from "react-hook-form";
 import { DevTool } from "react-hook-form-devtools";
 import { useHistory } from "react-router";
 import { errorNoti } from "../Notification";
+import { object, string } from "yup";
+import { toast } from "react-toastify";
 
 function EditVisitConfirguration(props) {
-  const history = useHistory();
   const dispatch = useDispatch();
+  const history = useHistory();
   const token = useSelector((state) => state.auth.token);
   const data = props.location.state;
 
-  // Form
+  // Form data.
+  const [weeks, setWeeks] = useState([]);
   const [frequencies, setFrequencies] = useState([]);
   const [configs] = useState(data.configs);
   const [respectiveConfigs, setRespectiveConfigs] = useState([
@@ -31,42 +34,115 @@ function EditVisitConfirguration(props) {
     },
   ]);
 
+  // Form engine.
+  const schema = object().shape({
+    frequency: string().required("Vui lòng chọn một mục"),
+    startExecuteWeek: string().when("config", {
+      is: (config) => config !== "" && config !== "None",
+      then: string().required("Vui lòng chọn một mục"),
+    }),
+  });
+
   const {
-    register,
     control,
     handleSubmit,
     errors,
-    getValues,
     setValue,
     watch,
+    clearError,
   } = useForm({
     defaultValues: {
-      frequency: data.frequency,
-      config: data.selectedConfig,
-      fromWeek: "",
+      frequency: data.visitFrequencyId,
+      config: data.salesRouteConfigId,
+      startExecuteWeek: data.startExecuteWeek,
     },
+    validationSchema: schema,
   });
 
-  const [payLoad, setPayLoad] = useState({
-    salesRouteConfigId: "",
-    visitFrequencyId: data.visitFrequencyId,
-  });
-
-  // Functions
+  // Functions.
   const getVisitFrequencies = () => {
-    authGet(dispatch, token, "/get-list-sales-route-visit-frequency").then(
-      (res) => {
-        console.log(res);
-        setFrequencies(res);
-      }
-    );
+    axiosGet(dispatch, token, "/get-list-sales-route-visit-frequency")
+      .then((res) => {
+        setFrequencies(res.data);
+        console.log("Frequencies ", res.data);
+      })
+      .catch((error) =>
+        console.log("Error in method getVisitFrequencies: ", error)
+      );
   };
 
-  const onChangeFrequency = (child) => {
-    setValue([{ config: "" }, { fromWeek: "" }]);
-    setPayLoad(() => ({ ...payLoad, visitFrequencyId: child.key }));
+  const generateListOfWeeks = () => {
+    console.time("generateListOfWeeks");
+
+    const fromDate = new Date(data.fromDate);
+    const toDate = new Date(data.toDate);
+
+    // Find the first Monday from fromDate to toDate.
+    const day = fromDate.getDay();
+    fromDate.setDate(
+      fromDate.getDate() + (day === 1 || day === 0 ? 1 - day : 8 - day)
+    );
+
+    // Generate list of weeks.
+    let monday;
+    let sunday;
+    let listOfWeeks = [];
+    let i = 1;
+
+    while (fromDate.getTime() <= toDate.getTime()) {
+      monday = `${("0" + fromDate.getDate()).slice(-2)}/${(
+        "0" +
+        (fromDate.getMonth() + 1)
+      ).slice(-2)}/${fromDate.getFullYear()}`;
+
+      fromDate.setDate(fromDate.getDate() + 6);
+
+      sunday = `${("0" + fromDate.getDate()).slice(-2)}/${(
+        "0" +
+        (fromDate.getMonth() + 1)
+      ).slice(-2)}/${fromDate.getFullYear()}`;
+
+      listOfWeeks.push({
+        id: i,
+        fromDate: monday,
+        toDate: sunday,
+      });
+
+      fromDate.setDate(fromDate.getDate() + 1);
+      i++;
+    }
+
+    // Last week may not be enough 7 days.
+    if (listOfWeeks.length > 0) {
+      const lastWeek = listOfWeeks[listOfWeeks.length - 1];
+      fromDate.setDate(fromDate.getDate() - 7);
+
+      listOfWeeks[listOfWeeks.length - 1] = {
+        id: lastWeek.id,
+
+        fromDate: `${("0" + fromDate.getDate()).slice(-2)}/${(
+          "0" +
+          (fromDate.getMonth() + 1)
+        ).slice(-2)}/${fromDate.getFullYear()}`,
+
+        toDate: `${("0" + toDate.getDate()).slice(-2)}/${(
+          "0" +
+          (toDate.getMonth() + 1)
+        ).slice(-2)}/${toDate.getFullYear()}`,
+      };
+    }
+
+    console.timeEnd("generateListOfWeeks");
+
+    setWeeks(listOfWeeks);
+  };
+
+  const onChangeFrequency = (visitFrequencyId) => {
+    setValue([{ config: "" }, { startExecuteWeek: "" }]);
+    clearError(["startExecuteWeek"]);
+
     setRespectiveConfigs([
-      ...configs.filter((c) => c.visitFrequencyId === child.key),
+      ...configs.filter((c) => c.visitFrequencyId === visitFrequencyId),
       {
         salesRouteConfigId: "None",
         days: "None",
@@ -74,9 +150,11 @@ function EditVisitConfirguration(props) {
     ]);
   };
 
-  const onChangeConfig = (child) => {
-    setValue("fromWeek", "");
-    setPayLoad(() => ({ ...payLoad, salesRouteConfigId: child.key }));
+  const onChangeConfig = (salesRouteConfigId) => {
+    if (salesRouteConfigId === "None") {
+      clearError(["startExecuteWeek"]);
+      setValue("startExecuteWeek", "");
+    }
   };
 
   const onClickCancelButton = () => {
@@ -84,30 +162,44 @@ function EditVisitConfirguration(props) {
   };
 
   const onSubmit = (formData) => {
+    console.log("Form data ", formData);
+    // notify()
+
     if (
-      formData.frequency !== data.frequency ||
-      formData.config !== data.selectedConfig
+      formData.frequency === data.visitFrequencyId &&
+      formData.config === data.salesRouteConfigId &&
+      formData.startExecuteWeek === data.startExecuteWeek
     ) {
-      authPut(dispatch, token, "/update-sales-route-config-retail-outlet", {
+      history.goBack();
+    } else {
+      axiosPut(dispatch, token, "/update-sales-route-config-retail-outlet", {
         salesRouteConfigRetailOutletId: data.salesRouteConfigRetailOutletId,
-        visitFrequencyId: payLoad.visitFrequencyId,
-        salesRouteConfigId:
-          payLoad.salesRouteConfigId === "None"
+        visitFrequencyId: formData.frequency,
+        salesRouteConfigId: formData.config === "None" ? null : formData.config,
+        startExecuteWeek:
+          formData.startExecuteWeek === "" ? null : formData.startExecuteWeek,
+        startExecuteDate:
+          formData.startExecuteWeek === ""
             ? null
-            : payLoad.salesRouteConfigId,
-        startExecuteWeek: formData.fromWeek,
-      }).then((res) => {
-        if (res.ok) {
+            : weeks[parseInt(formData.startExecuteWeek) - 1].fromDate.replace(
+                /\//g,
+                "-"
+              ),
+      })
+        .then((res) => {
+          toast.dismiss();
           history.goBack();
-        } else {
+        })
+        .catch((error) => {
           errorNoti();
-        }
-      });
+          console.log("Error in method onSubmit: ", error);
+        });
     }
   };
 
   useEffect(() => {
     getVisitFrequencies();
+    generateListOfWeeks();
   }, []);
 
   return (
@@ -125,42 +217,41 @@ function EditVisitConfirguration(props) {
             <br />
             <Typography variant="subtitle1" style={{ marginLeft: 30 }}>
               Đại lý bán lẻ: {data.retailOutletName}
-            </Typography>
-            <Typography variant="subtitle1" style={{ marginLeft: 30 }}>
+              <br />
               Nhân viên bán hàng: {data.salesmanName}
-            </Typography>
-            <Typography variant="subtitle1" style={{ marginLeft: 30 }}>
+              <br />
               Nhà phân phối: {data.distributorName}
+              <br />
             </Typography>
-            <br />
 
             {/* Form */}
             <form onSubmit={handleSubmit(onSubmit)}>
               <Controller
                 as={
                   <TextField
-                    required
                     select
                     error={!!errors.frequency}
                     id="frequency"
-                    label="Tần suất thăm"
-                    value={getValues("frequency")}
+                    label="Tần suất thăm*"
+                    value={watch("frequency")}
                     helperText={errors.frequency?.message}
                     style={{ minWidth: "200px", marginLeft: "30px" }}
                   >
                     {frequencies.map((f) => (
-                      <MenuItem key={f.visitFrequencyId} value={f.description}>
+                      <MenuItem
+                        key={f.visitFrequencyId}
+                        value={f.visitFrequencyId}
+                      >
                         {f.description}
                       </MenuItem>
                     ))}
                   </TextField>
                 }
                 name="frequency"
-                rules={{ required: "Vui lòng chọn một mục" }}
                 control={control}
-                onChange={(e) => {
-                  onChangeFrequency(e[1]);
-                  return e[1].props.value;
+                onChange={([, selected]) => {
+                  onChangeFrequency(selected.props.value);
+                  return selected.props.value;
                 }}
               />
               <br />
@@ -171,11 +262,14 @@ function EditVisitConfirguration(props) {
                     select
                     id="config"
                     label="Cấu hình thăm"
-                    value={getValues("config")}
+                    value={watch("config")}
                     style={{ minWidth: "200px", marginLeft: "30px" }}
                   >
                     {respectiveConfigs.map((c) => (
-                      <MenuItem key={c.salesRouteConfigId} value={c.days}>
+                      <MenuItem
+                        key={c.salesRouteConfigId}
+                        value={c.salesRouteConfigId}
+                      >
                         {c.days}
                       </MenuItem>
                     ))}
@@ -183,21 +277,36 @@ function EditVisitConfirguration(props) {
                 }
                 name="config"
                 control={control}
-                onChange={(e) => {
-                  onChangeConfig(e[1]);
-                  return e[1].props.value;
+                onChange={([, selected]) => {
+                  onChangeConfig(selected.props.value);
+                  return selected.props.value;
                 }}
               />
               <br />
               <br />
-              <TextField
-                disabled={watch("config") === "" || watch("config") === "None"}
-                id="fromWeek"
-                name="fromWeek"
-                label="Tuần bắt đầu"
-                helperText="Ví dụ: 1,2,3"
-                inputRef={register}
-                style={{ minWidth: "200px", marginLeft: "30px" }}
+              <Controller
+                as={
+                  <TextField
+                    select
+                    error={!!errors.startExecuteWeek}
+                    id="startExecuteWeek"
+                    label="Tuần bắt đầu"
+                    value={watch("startExecuteWeek")}
+                    helperText={errors.startExecuteWeek?.message}
+                    style={{ minWidth: "280px", marginLeft: "30px" }}
+                    disabled={
+                      watch("config") === "" || watch("config") === "None"
+                    }
+                  >
+                    {weeks.map((week) => (
+                      <MenuItem key={week.id} value={week.id}>
+                        {`Tuần ${week.id} (${week.fromDate} - ${week.toDate})`}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                }
+                name="startExecuteWeek"
+                control={control}
               />
               <br />
               <br />
