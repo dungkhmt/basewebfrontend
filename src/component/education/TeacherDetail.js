acea099c5bb70cbb0b380dfb02c5ec56df992c4f
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-08-02 09:41:36 +0700

    format code

--- src/component/education/TeacherDetail.js
+++ src/component/education/TeacherDetail.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function TeacherDetail(props){
-    return (
-        <div>
-            Hola
-        </div>
-    )
+export default function TeacherDetail(props) {
+  return <div>Teacher detail</div>;
 }
