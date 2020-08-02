23d31ae3c7c1911efd0072bf044a96fa30951ea1
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-08-02 09:45:21 +0700

    modify edu routes with new UI

--- src/routers/EduRoute.js
+++ src/routers/EduRoute.js
@@ -3,20 +3,31 @@ import { Route, Switch, useRouteMatch } from "react-router";
 import AssignmentList from "../component/education/AssignmentList";
 import BCASolver from "../component/education/BCASolver";
 import ClassesList from "../component/education/ClassesList";
-import CourseList from "../component/education/CoursesList";
 import CreateClass from "../component/education/CreateClass";
 import CreateSemester from "../component/education/CreateSemester";
 import TeacherDetail from "../component/education/TeacherDetail";
 import TeachersList from "../component/education/TeachersList";
+import CourseList from "../component/education/CourseList";
+import CourseDetail from "../component/education/CourseDetail";
+import AddNewCourse from "../component/education/AddNewCourse";
+import AddTeacher from "../component/education/AddTeacher";
 
 export default function EduRoute() {
   let { path, url } = useRouteMatch();
   return (
     <div>
       <Switch>
-        <Route component={CourseList} path={`${path}/courses-list`} />
+        <Route component={CourseList} path={`${path}/courses/list`} />
 
-        <Route component={TeachersList} path={`${path}/teachers-list`} />
+        <Route component={CourseDetail} path={`${path}/course/detail`} />
+
+        <Route component={AddNewCourse} path={`${path}/course/create`} />
+
+        <Route component={TeachersList} path={`${path}/teachers/list`} />
+
+        <Route component={TeacherDetail} path={`${path}/teacher/detail`} />
+
+        <Route component={AddTeacher} path={`${path}/teacher/create`} />
 
         <Route component={ClassesList} path={`${path}/classes-list`} />
 
@@ -27,8 +38,6 @@ export default function EduRoute() {
         <Route component={CreateSemester} path={`${path}/semester`} />
 
         <Route component={CreateClass} path={`${path}/create-class`} />
-
-        <Route component={TeacherDetail} path={`${path}/teacher-detail`} />
       </Switch>
     </div>
   );
