f42843afb3a38a0c4e137dd2a2821183610f46b3
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-10-10 22:52:10 +0700

    change icon of success noti

--- src/utils/Notification.js
+++ src/utils/Notification.js
@@ -5,6 +5,7 @@ import { GiInfo } from "react-icons/gi";
 import { FiCheckCircle } from "react-icons/fi";
 import { MdCancel, MdWarning } from "react-icons/md";
 import { Box } from "@material-ui/core";
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 
 // Snackbar
 
@@ -73,9 +74,10 @@ export const errorNoti = (message, autoClose) =>
 export const successNoti = (message, autoClose) =>
   toast.success(
     <Box display="flex" alignItems="center">
-      <IconContext.Provider>
-        <FiCheckCircle size={20} style={{colorl: 'white', marginRight: "5px" }} />
-      </IconContext.Provider>
+      {/* <IconContext.Provider>
+        <FiCheckCircle size={20} style={{marginRight: "5px" }} />
+      </IconContext.Provider> */}
+      <CheckCircleRoundedIcon style={{marginRight: "5px" }}/>
       {message}
     </Box>,
     {
