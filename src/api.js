e9d7f60cfa44e4fc70e8f5f8e54e1ba0cb42dde6
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-10-14 19:58:33 +0700

    add standardized API: request

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import { failed } from "./action/Auth";
 import { API_URL } from "./config/config";
 import axios from "axios";
+import { errorNoti, infoNoti } from "./utils/Notification";
 
 export const authPost = (dispatch, token, url, body) => {
   return fetch(API_URL + url, {
@@ -116,4 +117,91 @@ export const axiosPut = (token, url, data, dispatch) => {
       "X-Auth-Token": token,
     },
   });
+};
+
+const isFunction = (func) =>
+  func &&
+  (Object.prototype.toString.call(func) === "[object Function]" ||
+    "function" === typeof func ||
+    func instanceof Function);
+
+export const request = async (
+  token,
+  history,
+  method,
+  url,
+  successHandler,
+  errorHandlers,
+  data
+) => {
+  try {
+    const reqMethod = method.toUpperCase();
+    let res;
+
+    switch (reqMethod) {
+      case "GET":
+        res = await axios.get(API_URL + url, {
+          headers: {
+            "content-type": "application/json",
+            "X-Auth-Token": token,
+          },
+        });
+        break;
+      case "POST":
+        res = await axios.post(API_URL + url, data, {
+          headers: {
+            "content-type": "application/json",
+            "X-Auth-Token": token,
+          },
+        });
+        break;
+      case "PUT":
+        res = axios.put(API_URL + url, data, {
+          headers: {
+            "content-type": "application/json",
+            "X-Auth-Token": token,
+          },
+        });
+        break;
+    }
+
+    if (isFunction(successHandler)) {
+      successHandler(res);
+    }
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code that falls out of the range of 2xx.
+      switch (error.response.status) {
+        case 401:
+          history.push({ pathname: "/login" });
+          break;
+        case 403:
+          infoNoti("Bạn cần được cấp quyền để thực hiện hành động này.");
+          break;
+        default:
+          if (isFunction(errorHandlers[error.response.status])) {
+            errorHandlers[error.response.status](error);
+          } else if (isFunction(errorHandlers["rest"])) {
+            errorHandlers["rest"](error);
+          } else {
+            errorNoti("Rất tiếc! Đã có lỗi xảy ra.");
+          }
+      }
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(
+        "The request was made but no response was received",
+        error.request
+      );
+    } else {
+      // Something happened in setting up the request that triggered an Error.
+      console.log(
+        "Something happened in setting up the request that triggered an Error",
+        error.message
+      );
+    }
+    console.log(error.config);
+  }
 };
