701dca6642591e6866f97047d9cc830c64126b8d
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-11-02 13:03:20 +0700

    upgrade API: request

--- src/api.js
+++ src/api.js
@@ -131,7 +131,7 @@ export const request = async (
   method,
   url,
   successHandler,
-  errorHandlers,
+  errorHandlers = {},
   data
 ) => {
   try {
@@ -148,44 +148,49 @@ export const request = async (
     if (isFunction(successHandler)) {
       successHandler(res);
     }
-  } catch (error) {
-    if (error.response) {
+  } catch (e) {
+    // Handling work to do when encountering all kinds of errors, e.g turn off the loading icon.
+    if (isFunction(errorHandlers['commonTask'])) {
+      errorHandlers['commonTask'](e)
+    }
+
+    if (e.response) {
       // The request was made and the server responded with a status code that falls out of the range of 2xx.
-      switch (error.response.status) {
+      switch (e.response.status) {
         case 401:
           if(isFunction(errorHandlers[401])){
-            errorHandlers[401](error)
+            errorHandlers[401](e)
           } else{
             history.push({ pathname: "/login" });
           }
           break;
         case 403:
           if(isFunction(errorHandlers[403])){
-            errorHandlers[403](error)
+            errorHandlers[403](e)
           } else{
             infoNoti("Bạn cần được cấp quyền để thực hiện hành động này.");
           }
           break;
         default:
-          if (isFunction(errorHandlers[error.response.status])) {
-            errorHandlers[error.response.status](error);
+          if (isFunction(errorHandlers[e.response.status])) {
+            errorHandlers[e.response.status](e);
           } else if (isFunction(errorHandlers["rest"])) {
-            errorHandlers["rest"](error);
+            errorHandlers["rest"](e);
           } else {
             errorNoti("Rất tiếc! Đã có lỗi xảy ra.");
           }
       }
-    } else if (error.request) {
+    } else if (e.request) {
       // The request was made but no response was received
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
       console.log(
         "The request was made but no response was received",
-        error.request
+        e.request
       );
 
       if(isFunction(errorHandlers['noResponse'])) {
-        errorHandlers['noResponse'](error)
+        errorHandlers['noResponse'](e)
       }
 
       errorNoti("Không thể kết nối tới máy chủ.");
@@ -193,9 +198,9 @@ export const request = async (
       // Something happened in setting up the request that triggered an Error.
       console.log(
         "Something happened in setting up the request that triggered an Error",
-        error.message
+        e.message
       );
     }
-    console.log('Request config', error.config);
+    console.log('Request config', e.config);
   }
 };
