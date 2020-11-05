daf28634c8f1200ab8aae8dae087a7075ce6f511
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-11-05 18:04:08 +0700

    styled button

---
+++ src/component/education/classmanagement/PositiveButton.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core";
+
+const styles = {
+  root: {
+    minWidth: 124,
+    borderRadius: "6px",
+    backgroundColor: "#1877f2",
+    textTransform: "none",
+    fontSize: "1rem",
+    fontWeight: 600,
+    "&:hover": {
+      backgroundColor: "#1834d2",
+    },
+  },
+};
+
+function PosButton(props) {
+  const { label, className } = props;
+  return (
+    <Button {...props} className={className}>
+      {label}
+    </Button>
+  );
+}
+
+export default withStyles(styles)(PosButton);
