6db88547117e75c050a63d4a0fbd2228ba40a9d8
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-02-24 21:56:54 +0700

    add feature: download program submission

--- src/component/education/programmingcontest/ProgramSubmissionTable.js
+++ src/component/education/programmingcontest/ProgramSubmissionTable.js
@@ -1,68 +1,131 @@
 import {
-    Button, Card, CardActions, CardContent, TextField, Typography,
-    MenuItem, Checkbox, 
-  } from "@material-ui/core/";
-  import React, { useState, useEffect } from "react";
-  import { useHistory } from "react-router-dom";
-  import { authPost, authGet, authPostMultiPart } from "../../../api";
-  import { useDispatch, useSelector } from "react-redux";
-  import { useParams } from "react-router";
-  import MaterialTable from "material-table";
-  import {Link} from "react-router-dom";
-  
-function ProgramSubmissionTable(){
-    const params = useParams();
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.auth.token);
-    const history = useHistory();
-    const [programSubmissions, setProgramSubmissions] = useState([]);
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Typography,
+  MenuItem,
+  Checkbox,
+} from "@material-ui/core/";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { authPost, authGet, authPostMultiPart, request } from "../../../api";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import MaterialTable from "material-table";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../../../config/config";
 
-    const columnSubmissions = [
-        { title: 'ID bài tập', field: 'problemId', 
-          render: rowData => (
-            <Link to={"/edu/contest-problem/detail/" + rowData["problemId"]}>
-            {rowData["problemId"]}
-            </Link>
-          )
-        },
-        { title: 'Tên bài tập', field: 'problemName' },
-        { title: 'Người nộp', field: 'submittedByUserLoginId' },
-        { title: 'Thời gian nộp', field: 'createdStamp' },     
-        { title: 'Points', field: 'points' },     
-        { title: 'ID', field: 'contestProgramSubmissionId', 
-          render: rowData => (
-            <Link to={"/edu/contest-program-submission/detail/" + rowData["contestProgramSubmissionId"]}>
-            {rowData["contestProgramSubmissionId"]}
-            </Link>
-          )
-        },
-      ];
+function ProgramSubmissionTable() {
+  const params = useParams();
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const history = useHistory();
+  const [programSubmissions, setProgramSubmissions] = useState([]);
 
-      async function getContestProgramSubmissionList(){
-        let submissions = await authGet(dispatch, token, '/get-all-contest-program-submissions');
-        setProgramSubmissions(submissions);
-        
+  const columnSubmissions = [
+    {
+      title: "ID bài tập",
+      field: "problemId",
+      render: (rowData) => (
+        <Link to={"/edu/contest-problem/detail/" + rowData["problemId"]}>
+          {rowData["problemId"]}
+        </Link>
+      ),
+    },
+    { title: "Tên bài tập", field: "problemName" },
+    { title: "Người nộp", field: "submittedByUserLoginId" },
+    { title: "Thời gian nộp", field: "createdStamp" },
+    { title: "Points", field: "points" },
+    {
+      title: "ID",
+      field: "contestProgramSubmissionId",
+      render: (rowData) => (
+        <Button
+          color="primary"
+          variant="outlined"
+          onClick={() => onDownload(rowData["contestProgramSubmissionId"])}
+        >
+          Tải xuống
+        </Button>
+      ),
+    },
+  ];
+
+  const onDownload = (submissionId) => {
+    axios
+      .get(`${API_URL}/get-detail-contest-program-submission/${submissionId}`, {
+        headers: {
+          "content-type": "application/json",
+          "X-Auth-Token": token,
+        },
+        responseType: "blob",
+      })
+      .then((res) => {
+        const headerval = res.headers["content-disposition"];
+        const filename = headerval
+          .split(";")[1]
+          .split("=")[1]
+          .replace('"', "")
+          .replace('"', "");
+
+        saveFile(filename, res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const saveFile = (fileName, data) => {
+    let blob = new Blob([data]);
+
+    //IE11 support
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, fileName);
+    } else {
+      let link = window.document.createElement("a");
+
+      link.href = window.URL.createObjectURL(blob);
+      link.setAttribute("download", fileName);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      // other browsers
+      // Second approach but cannot specify saved name!
+      // let file = new File([data], fileName, { type: "application/zip" });
+      // let exportUrl = URL.createObjectURL(file);
+      // window.location.assign(exportUrl);
+      // URL.revokeObjectURL(exportUrl);
     }
-  
-      useEffect(() => {
-          
-          getContestProgramSubmissionList();
-        }, []);
-     
+  };
 
-    return(
-        <Card>
-            <CardContent>
-            <MaterialTable
-                title={"Danh sách Bài nộp"}
-                columns={columnSubmissions}
-                data = {programSubmissions}    
-                />  
-
-                
-            </CardContent>
-        </Card>
+  async function getContestProgramSubmissionList() {
+    let submissions = await authGet(
+      dispatch,
+      token,
+      "/get-all-contest-program-submissions"
     );
+    setProgramSubmissions(submissions);
+  }
+
+  useEffect(() => {
+    getContestProgramSubmissionList();
+  }, []);
+
+  return (
+    <Card>
+      <CardContent>
+        <MaterialTable
+          title={"Danh sách Bài nộp"}
+          columns={columnSubmissions}
+          data={programSubmissions}
+        />
+      </CardContent>
+    </Card>
+  );
 }
 
 export default ProgramSubmissionTable;
