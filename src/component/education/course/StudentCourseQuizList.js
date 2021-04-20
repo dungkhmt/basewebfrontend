a459b66e0cf30c98850ecead70f38a1c9222760c
dungpq <dungkhmt@gmail.com>
2021-04-20 12:42:33 +0700

    upgrade student quiz list

--- src/component/education/course/StudentCourseQuizList.js
+++ src/component/education/course/StudentCourseQuizList.js
@@ -1,61 +1,8 @@
-import {
-    Button, Card, CardActions, CardContent, TextField, Typography,
-    MenuItem, Checkbox, 
-  } from "@material-ui/core/";
-  import React, { useState, useEffect } from "react";
-  import { useHistory } from "react-router-dom";
-  import { authPost, authGet, authPostMultiPart } from "../../../api";
-  import { useDispatch, useSelector } from "react-redux";
-  import { useParams } from "react-router";
-  import MaterialTable from "material-table";
-  import {Link} from "react-router-dom";
-  import AddIcon from '@material-ui/icons/Add';
+import React from "react";
+import StudentQuizView from "./StudentQuizView";
 
-function StudentCourseQuizList(props){
-    const params = useParams();
-    const courseId = props.courseId;
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.auth.token);
-    const history = useHistory();
-    const [quizs, setQuizs] = useState([]);
-
-    const columns = [
-        { title: 'QuizId', field: 'questionId',
-            render: rowData => (
-                <Link to={"/edu/student/course/chapter/detail/" + rowData["questionId"]}>
-                {rowData["questionId"]}
-                </Link>
-            )
-        },
-        { title: 'Statement', field: 'statement'}
-
-       
-      ];
-
-    async function getQuizList(){
-        let lst = await authGet(dispatch, token, '/get-quiz-of-course/' + courseId);
-        setQuizs(lst);        
-    }
-  
-      useEffect(() => {
-        console.log("StudentCourseQuizList, useEffect, courseId = ",courseId)  
-        getQuizList();
-        
-        }, []);
-     
-    return (
-        <Card>
-            <CardContent>
-            <MaterialTable
-                title={"Quiz"}
-                columns={columns}
-                data = {quizs}    
-                
-                />                
-            </CardContent>
-        </Card>
-    );
-  
+function StudentCourseQuizList({ quizzList }) {
+  return quizzList.map((q) => <StudentQuizView quizz={q} />);
 }
 
 export default StudentCourseQuizList;
