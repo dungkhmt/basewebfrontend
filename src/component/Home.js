dc5d04c279c2eabc2b43bbaae10a1c79a67660ff
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-04 09:31:41 +0700

    fix bug quizz chart

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,14 @@
-import React, {useEffect, useState} from "react";
-import {HorizontalBar} from "react-chartjs-2";
 import Grid from "@material-ui/core/Grid";
-import {authPost} from "../api";
-import {useDispatch, useSelector} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { HorizontalBar } from "react-chartjs-2";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { authPost, request } from "../api";
 
 export default function Home(props) {
   const dispatch = useDispatch();
-  const token = useSelector(state => state.auth.token);
+  const token = useSelector((state) => state.auth.token);
+  const history = useHistory();
 
   const [vehicle, setVehicle] = useState([]);
   const [distance, setDistance] = useState([]);
@@ -19,166 +21,200 @@ export default function Home(props) {
 
   const [dateQuizParticipation, setDateQuizParticipation] = useState([]);
   const [totalQuizParticipation, setTotalQuizParticipation] = useState([]);
-  
+
   const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: 'My First dataset',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
+        label: "My First dataset",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
   };
 
   const dataRevenue = {
     labels: dateRevenue,
     datasets: [
       {
-        label: 'Revenue',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
+        label: "Revenue",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: revenue
-      }
-    ]
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: revenue,
+      },
+    ],
   };
 
   const dataVehicleDistance = {
     labels: vehicle,
     datasets: [
       {
-        label: 'Distance of vehicle',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
+        label: "Distance of vehicle",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: distance
-      }
-    ]
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: distance,
+      },
+    ],
   };
 
-const dataStudentParticipation = {
+  const dataStudentParticipation = {
     labels: dateStudentParticipation,
     datasets: [
       {
-        label: 'Participation',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
+        label: "Participation",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: totalParticipation
-      }
-    ]
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: totalParticipation,
+      },
+    ],
   };
 
-const dataQuizParticipation = {
+  const dataQuizParticipation = {
     labels: dateQuizParticipation,
     datasets: [
       {
-        label: 'Quiz Participation',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
+        label: "Quiz Participation",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: totalQuizParticipation
-      }
-    ]
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: totalQuizParticipation,
+      },
+    ],
   };
 
-  async function getStudentParticipation(){
-    let lst = await authPost(dispatch, token, '/get-class-participation-statistic', {"fromDate": "", "thruDate":""});
-    let dates = [];
-    let participations = [];
-    console.log('getStudentParticipation, lst = ', lst);
-    lst.forEach(r => {
-      dates.push(r.date);
-      participations.push(r.count);
-    });
-    setDateStudentParticipation(dates);
-    setTotalParticipation(participations);
-    
+  async function getStudentParticipation() {
+    request(
+      token,
+      history,
+      "post",
+      "/get-class-participation-statistic",
+      (res) => {
+        let lst = res.data;
+        let dates = [];
+        let participations = [];
+
+        console.log("getStudentParticipation, lst = ", lst);
+
+        lst.forEach((r) => {
+          dates.push(r.date);
+          participations.push(r.count);
+        });
+
+        setDateStudentParticipation(dates);
+        setTotalParticipation(participations);
+      },
+      {},
+      {
+        fromDate: "",
+        thruDate: "",
+      }
+    );
   }
-  async function getQuizParticipation(){
-    let lst = await authPost(dispatch, token, '/get-quiz-participation-statistic', {"fromDate": "", "thruDate":""});
-    let dates = [];
-    let participations = [];
-    console.log('getQuizParticipation, lst = ', lst);
-    lst.forEach(r => {
-      dates.push(r.date);
-      participations.push(r.count);
-    });
-    setDateQuizParticipation(dates);
-    setTotalQuizParticipation(participations);
-    
+  async function getQuizParticipation() {
+    request(
+      token,
+      history,
+      "post",
+      "/get-quiz-participation-statistic",
+      (res) => {
+        let lst = res.data;
+        let dates = [];
+        let participations = [];
+
+        console.log("getQuizParticipation, lst = ", lst);
+
+        lst.forEach((r) => {
+          dates.push(r.date);
+          participations.push(r.count);
+        });
+
+        setDateQuizParticipation(dates);
+        setTotalQuizParticipation(participations);
+      },
+      {},
+      { fromDate: "", thruDate: "" }
+    );
   }
-  
 
   function getRevenueDateRecent() {
     console.log("getRevenueDateRecent");
-    authPost(dispatch, token, '/report-date-based-revenue-recent', {"nbDays": 15}
-    ).then(
-      response => response.json()
-    ).then(response => {
-      console.log('response = ', response);
-      let listRev = response.revenueElements;
-      //setVehicleDistance(response);
-      let arrDates = [];
-      let arrRevenues = [];
-      listRev.forEach(r => {
-        arrDates.push(r.date);
-        arrRevenues.push(r.revenue);
-      });
-      setDateRevenue(arrDates);
-      setRevenue(arrRevenues);
-      //dataVehicleDistance.labels = vehicle;
-      //dataVehicleDistance.datasets[0].data = distance;
-      //console.log('dataVehicleDistance.vehicle = ', dataVehicleDistance.labels);
-      //console.log('dataVehicleDistance.distance = ', dataVehicleDistance.datasets[0].data);
-      console.log('revenue = ', revenue);
-    }).catch(console.log);
-
-
+    authPost(dispatch, token, "/report-date-based-revenue-recent", {
+      nbDays: 15,
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("response = ", response);
+        let listRev = response.revenueElements;
+        //setVehicleDistance(response);
+        let arrDates = [];
+        let arrRevenues = [];
+        listRev.forEach((r) => {
+          arrDates.push(r.date);
+          arrRevenues.push(r.revenue);
+        });
+        setDateRevenue(arrDates);
+        setRevenue(arrRevenues);
+        //dataVehicleDistance.labels = vehicle;
+        //dataVehicleDistance.datasets[0].data = distance;
+        //console.log('dataVehicleDistance.vehicle = ', dataVehicleDistance.labels);
+        //console.log('dataVehicleDistance.distance = ', dataVehicleDistance.datasets[0].data);
+        console.log("revenue = ", revenue);
+      })
+      .catch(console.log);
   }
 
   function getVehicleDistance() {
     console.log("getVehicleDistance");
-    authPost(dispatch, token, '/statistic-vehicle-distance', {"fromDate": "", "thruDate": ""}
-    ).then(
-      response => response.json()
-    ).then(response => {
-      console.log('response = ', response);
-      //setVehicleDistance(response);
-      let vehicle = [];
-      let distance = [];
-      response.forEach(vh => {
-        vehicle.push(vh.vehicleId);
-        distance.push(vh.distance);
-      });
-      setVehicle(vehicle);
-      setDistance(distance);
-      //dataVehicleDistance.labels = vehicle;
-      //dataVehicleDistance.datasets[0].data = distance;
-      console.log('dataVehicleDistance.vehicle = ', dataVehicleDistance.labels);
-      console.log('dataVehicleDistance.distance = ', dataVehicleDistance.datasets[0].data);
-    }).catch(console.log);
-
+    authPost(dispatch, token, "/statistic-vehicle-distance", {
+      fromDate: "",
+      thruDate: "",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("response = ", response);
+        //setVehicleDistance(response);
+        let vehicle = [];
+        let distance = [];
+        response.forEach((vh) => {
+          vehicle.push(vh.vehicleId);
+          distance.push(vh.distance);
+        });
+        setVehicle(vehicle);
+        setDistance(distance);
+        //dataVehicleDistance.labels = vehicle;
+        //dataVehicleDistance.datasets[0].data = distance;
+        console.log(
+          "dataVehicleDistance.vehicle = ",
+          dataVehicleDistance.labels
+        );
+        console.log(
+          "dataVehicleDistance.distance = ",
+          dataVehicleDistance.datasets[0].data
+        );
+      })
+      .catch(console.log);
   }
 
   useEffect(() => {
     getVehicleDistance();
     getRevenueDateRecent();
     getStudentParticipation();
-     getQuizParticipation();
+    getQuizParticipation();
   }, []);
 
   return (
@@ -186,26 +222,22 @@ const dataQuizParticipation = {
       <Grid container spacing={3}>
         <Grid item xs={6}>
           <h2>Thống kê học course</h2>
-          <HorizontalBar data={dataStudentParticipation}/>
+          <HorizontalBar data={dataStudentParticipation} />
         </Grid>
         <Grid item xs={6}>
           <h2>Thống kê làm quiz</h2>
-          <HorizontalBar data={dataQuizParticipation}/>
+          <HorizontalBar data={dataQuizParticipation} />
         </Grid>
 
         <Grid item xs={6}>
           <h2>Thống kê doanh số</h2>
-          <HorizontalBar data={dataRevenue}/>
+          <HorizontalBar data={dataRevenue} />
         </Grid>
         <Grid item xs={6}>
           <h2>Thống kê khoảng cách di chuyển các xe</h2>
-          <HorizontalBar data={dataVehicleDistance}/>
+          <HorizontalBar data={dataVehicleDistance} />
         </Grid>
-        
-        
       </Grid>
     </div>
   );
-
-
 }
