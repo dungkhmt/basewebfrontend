57169d73c3a133ac46c4fc0b9969ac344df02d55
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-04 20:28:02 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import appReducer from "./reducers";
-import { applyMiddleware, createStore } from "redux";
-import thunkMiddleware from "redux-thunk";
 import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import { LOGOUT_SUCCESS } from "./action/Auth";
+import App from "./App";
+import "./index.css";
+import appReducer from "./reducers";
+import * as serviceWorker from "./serviceWorker";
 
 const loggerMiddleware = createLogger();
 let middleware = [
