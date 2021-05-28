83ceaeb2125ff10c712adc9c1c33ed5935c18314
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-28 17:00:16 +0700

    rm unused code

--- src/component/education/course/TeacherCourseQuizDetail.js
+++ src/component/education/course/TeacherCourseQuizDetail.js
@@ -1,40 +1,26 @@
+import { Card } from "@material-ui/core/";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { useHistory } from "react-router-dom";
+import TeacherCourseQuizChoiceAnswerList from "./TeacherCourseQuizChoiceAnswerList";
+import TeacherCourseQuizContent from "./TeacherCourseQuizContent";
+function TeacherCourseQuizDetail() {
+  const params = useParams();
+  const questionId = params.questionId;
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const history = useHistory();
+  const [courseChapter, setCourseChapter] = useState(null);
 
+  useEffect(() => {}, []);
 
-import {
-    Button, Card, CardActions, CardContent, TextField, Typography,
-    MenuItem, Checkbox, 
-  } from "@material-ui/core/";
-  import React, { useState, useEffect } from "react";
-  import { useHistory } from "react-router-dom";
-  import { authPost, authGet, authPostMultiPart } from "../../../api";
-  import { useDispatch, useSelector } from "react-redux";
-  import { useParams } from "react-router";
-  import MaterialTable from "material-table";
-  import {Link} from "react-router-dom";
-  import TeacherCourseChapterMaterialList from "./TeacherCourseChapterMaterialList";
-  import TeacherCourseQuizChoiceAnswerList from "./TeacherCourseQuizChoiceAnswerList";
-  import TeacherCourseQuizContent from "./TeacherCourseQuizContent";
-function TeacherCourseQuizDetail(){
-    const params = useParams();
-    const questionId = params.questionId;
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.auth.token);
-    const history = useHistory();
-    const [courseChapter, setCourseChapter] = useState(null);
-    
-    useEffect(() => {
-          
-        
-        }, []);
-
-    return(
-        <Card>
-            <CardContent>
-                <TeacherCourseQuizContent questionId={questionId}/>
-                <TeacherCourseQuizChoiceAnswerList questionId={questionId}/>
-            </CardContent>
-        </Card>
-    );
+  return (
+    <Card>
+      <TeacherCourseQuizContent questionId={questionId} />
+      <TeacherCourseQuizChoiceAnswerList questionId={questionId} />
+    </Card>
+  );
 }
 
 export default TeacherCourseQuizDetail;
