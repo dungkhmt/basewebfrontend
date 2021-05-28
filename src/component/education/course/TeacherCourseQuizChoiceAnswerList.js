f18dacd39ea33de92afed3819a16d42660975f65
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-28 17:07:52 +0700

    add parse HTML

--- src/component/education/course/TeacherCourseQuizChoiceAnswerList.js
+++ src/component/education/course/TeacherCourseQuizChoiceAnswerList.js
@@ -1,70 +1,85 @@
-import {
-    Button, Card, CardActions, CardContent, TextField, Typography,
-    MenuItem, Checkbox, 
-  } from "@material-ui/core/";
-  import React, { useState, useEffect } from "react";
-  import { useHistory } from "react-router-dom";
-  import { authPost, authGet, authPostMultiPart } from "../../../api";
-  import { useDispatch, useSelector } from "react-redux";
-  import { useParams } from "react-router";
-  import MaterialTable from "material-table";
-  import {Link} from "react-router-dom";
-  import AddIcon from '@material-ui/icons/Add';
+import { Card, CardContent, Typography } from "@material-ui/core/";
+import AddIcon from "@material-ui/icons/Add";
+import parse from "html-react-parser";
+import MaterialTable from "material-table";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { Link, useHistory } from "react-router-dom";
+import { authGet } from "../../../api";
 
-function TeacherCourseQuizChoiceAnswerList(props){
-    const params = useParams();
-    const questionId = props.questionId;
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.auth.token);
-    const history = useHistory();
-    const [choiceAnswers, setChoiceAnswers] = useState([]);
+function TeacherCourseQuizChoiceAnswerList(props) {
+  const params = useParams();
+  const questionId = props.questionId;
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const history = useHistory();
+  const [choiceAnswers, setChoiceAnswers] = useState([]);
 
-    const columns = [
-        { title: 'choiceAnswerId', field: 'choiceAnswerId',
-            render: rowData => (
-                <Link to={"/edu/teacher/course/quiz/choiceanswer/detail/" + rowData["choiceAnswerId"]}>
-                {rowData["choiceAnswerId"]}
-                </Link>
-            )
-        },
-        { title: 'Content', field: 'choiceAnswerContent'},
-        { title: 'isCorrectAnswer', field: 'isCorrectAnswer'}
-       
-      ];
+  const columns = [
+    {
+      title: "choiceAnswerId",
+      field: "choiceAnswerId",
+      render: (rowData) => (
+        <Link
+          to={
+            "/edu/teacher/course/quiz/choiceanswer/detail/" +
+            rowData["choiceAnswerId"]
+          }
+        >
+          {rowData["choiceAnswerId"]}
+        </Link>
+      ),
+    },
+    {
+      title: "Content",
+      field: "choiceAnswerContent",
+      render: (rowData) => (
+        <Typography>{parse(rowData.choiceAnswerContent)}</Typography>
+      ),
+    },
+    { title: "isCorrectAnswer", field: "isCorrectAnswer" },
+  ];
 
-    async function getChoiceAnswerList(){
-
-        let lst = await authGet(dispatch, token, '/get-quiz-choice-answer-of-a-quiz/' + questionId);
-        setChoiceAnswers(lst);        
-        console.log('getCHoiceAnsweList, questionId = ' + questionId);
-    }
-  
-      useEffect(() => {
-          
-        getChoiceAnswerList();
-        
-        }, []);
-     
-    return (
-        <Card>
-            <CardContent>
-            <MaterialTable
-                title={"Choice Answer"}
-                columns={columns}
-                data = {choiceAnswers}    
-                actions={[
-                    {
-                      icon: () => { return <AddIcon color='primary' fontSize='large' /> },
-                      tooltip: 'Thêm mới',
-                      isFreeAction: true,
-                      onClick: () => { history.push('/edu/course/detail/quiz/choiceanswer/create/' + questionId) }
-                    },
-                  ]}
-                />                
-            </CardContent>
-        </Card>
+  async function getChoiceAnswerList() {
+    let lst = await authGet(
+      dispatch,
+      token,
+      "/get-quiz-choice-answer-of-a-quiz/" + questionId
     );
+    setChoiceAnswers(lst);
+    console.log("getCHoiceAnsweList, questionId = " + questionId);
+  }
 
+  useEffect(() => {
+    getChoiceAnswerList();
+  }, []);
+
+  return (
+    <Card>
+      <CardContent>
+        <MaterialTable
+          title={"Choice Answer"}
+          columns={columns}
+          data={choiceAnswers}
+          actions={[
+            {
+              icon: () => {
+                return <AddIcon color="primary" fontSize="large" />;
+              },
+              tooltip: "Thêm mới",
+              isFreeAction: true,
+              onClick: () => {
+                history.push(
+                  "/edu/course/detail/quiz/choiceanswer/create/" + questionId
+                );
+              },
+            },
+          ]}
+        />
+      </CardContent>
+    </Card>
+  );
 }
 
 export default TeacherCourseQuizChoiceAnswerList;
