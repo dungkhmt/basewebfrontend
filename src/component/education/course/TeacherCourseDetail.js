ccda065a30c0e73c702649dfeb3fb17b95264795
dungpq <dungkhmt@gmail.com>
2021-05-28 16:47:50 +0700

    upgrade quiz test

--- src/component/education/course/TeacherCourseDetail.js
+++ src/component/education/course/TeacherCourseDetail.js
@@ -1,41 +1,48 @@
-
 import {
-    Button, Card, CardActions, CardContent, TextField, Typography,
-    MenuItem, Checkbox, 
-  } from "@material-ui/core/";
-  import React, { useState, useEffect } from "react";
-  import { useHistory } from "react-router-dom";
-  import { authPost, authGet, authPostMultiPart } from "../../../api";
-  import { useDispatch, useSelector } from "react-redux";
-  import { useParams } from "react-router";
-  import MaterialTable from "material-table";
-  import {Link} from "react-router-dom";
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Typography,
+  MenuItem,
+  Checkbox,
+} from "@material-ui/core/";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { authPost, authGet, authPostMultiPart } from "../../../api";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import MaterialTable from "material-table";
+import { Link } from "react-router-dom";
 import TeacherCourseChapterList from "./TeacherCourseChapterList";
 import TeacherCourseQuizList from "./TeacherCourseQuizList";
+import TeacherViewCourseQuizList from "./TeacherViewCourseQuizList";
+
 import TeacherCourseTopicList from "./TeacherCourseTopicList";
 
-function TeacherCourseDetail(){
-    const params = useParams();
-    const courseId = params.id;
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.auth.token);
-    const history = useHistory();
-    const [course, setCourse] = useState(null);
-    
-    useEffect(() => {
-          
-        console.log(courseId);
-        }, []);
+function TeacherCourseDetail() {
+  const params = useParams();
+  const courseId = params.id;
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const history = useHistory();
+  const [course, setCourse] = useState(null);
 
-    return(
-        <Card>
-            <CardContent>
-                <TeacherCourseChapterList courseId={courseId}/>
-                <TeacherCourseQuizList courseId={courseId}/>
-                <TeacherCourseTopicList courseId={courseId}/>
-            </CardContent>
-        </Card>
-    );
+  useEffect(() => {
+    console.log(courseId);
+  }, []);
+
+  return (
+    <Card>
+      <CardContent>
+        <TeacherCourseChapterList courseId={courseId} />
+        <TeacherCourseQuizList courseId={courseId} />
+        <TeacherCourseTopicList courseId={courseId} />
+        <TeacherViewCourseQuizList courseId={courseId} />
+      </CardContent>
+    </Card>
+  );
 }
 
 export default TeacherCourseDetail;
