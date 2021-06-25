8881968e93788ee7bf609583061415abfd3df0c9
dungpq <dungkhmt@gmail.com>
2021-06-25 17:35:40 +0700

    add preassign solution

--- src/component/education/classteacherassignment/ClassTeacherAssignmentSolutionList.js
+++ src/component/education/classteacherassignment/ClassTeacherAssignmentSolutionList.js
@@ -1,8 +1,25 @@
-import { Button, Card } from "@material-ui/core/";
+import { Button, Card, Checkbox, Tooltip } from "@material-ui/core/";
 import MaterialTable, { MTableToolbar } from "material-table";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { request } from "../../../api";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+
+const headerProperties = {
+  headerStyle: {
+    fontSize: 16,
+    backgroundColor: "rgb(63, 81, 181)",
+    color: "white",
+  },
+};
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
+let count = 0;
 
 function ClassTeacherAssignmentSolutionList(props) {
   const { planId, planName } = props;
@@ -11,7 +28,8 @@ function ClassTeacherAssignmentSolutionList(props) {
   const [isProcessing, setIsProcessing] = useState(false);
   const dispatch = useDispatch();
   const token = useSelector((state) => state.auth.token);
-  //const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedAll, setSelectedAll] = useState(false);
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
   const columns = [
     { title: "Mã lớp", field: "classCode" },
@@ -34,6 +52,31 @@ function ClassTeacherAssignmentSolutionList(props) {
         </Button>
       ),
     },
+    {
+      field: "selected",
+      title: "Chọn",
+
+      width: "10%",
+      type: "numeric",
+      render: (rowData) => (
+        <Checkbox
+          checked={rowData.selected}
+          onChange={(e) => {
+            rowData.selected = e.target.checked;
+            if (rowData.selected == false) {
+              count--;
+              setSelectedAll(false);
+            } else {
+              count++;
+            }
+            if (count == classList.length) {
+              setSelectedAll(true);
+            }
+            forceUpdate();
+          }}
+        />
+      ),
+    },
   ];
 
   async function getClassTeacherAssignmentSolutionList() {
@@ -43,7 +86,22 @@ function ClassTeacherAssignmentSolutionList(props) {
       "GET",
       "/get-class-teacher-assignment-solution/" + planId,
       (res) => {
-        setClassList(res.data);
+        let temp = [];
+        res.data.map((elm, index) => {
+          temp.push({
+            solutionItemId: elm.solutionItemId,
+            classCode: elm.classCode,
+            courseId: elm.courseId,
+            courseName: elm.courseName,
+            teacherId: elm.teacherId,
+            teacherName: elm.teacherName,
+            timetable: elm.timetable,
+            hourLoad: elm.hourLoad,
+            selected: false,
+          });
+        });
+        setClassList(temp);
+        //setClassList(res.data);
       }
     );
   }
@@ -69,6 +127,45 @@ function ClassTeacherAssignmentSolutionList(props) {
       { 401: () => {} },
       datasend
     );
+  }
+
+  function handleRemoveClassTeacherAssignmentSolution(e) {
+    let acceptList = [];
+    classList.map((v, i) => {
+      if (v.selected == true) {
+        acceptList.push(
+          JSON.stringify({
+            solutionItemId: v.solutionItemId,
+          })
+        );
+      }
+    });
+
+    if (acceptList.length != 0) {
+      let result = -1;
+      let formData = new FormData();
+      formData.append("planId", planId);
+      formData.append("solutionItemList", acceptList.join(";"));
+      request(
+        // token,
+        // history,
+        "POST",
+        "/remove-class-teacher-assign-solution-list",
+        (res) => {
+          result = res.data;
+
+          if (result >= 0) {
+            let temp = classList.filter(
+              (el) => !acceptList.includes(el.userLoginId)
+            );
+            setClassList(temp);
+            count = 0;
+          }
+        },
+        {},
+        formData
+      );
+    }
   }
 
   const saveFile = (fileName, data) => {
@@ -130,6 +227,66 @@ function ClassTeacherAssignmentSolutionList(props) {
             </div>
           ),
         }}
+        actions={[
+          {
+            icon: () => {
+              return (
+                <Tooltip
+                  title="Loại thí sinh khỏi kì thi"
+                  aria-label="Loại thí sinh khỏi kì thi"
+                  placement="top"
+                >
+                  <ThemeProvider theme={theme} style={{ color: "white" }}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={(e) => {
+                        handleRemoveClassTeacherAssignmentSolution(e);
+                      }}
+                      style={{ color: "white" }}
+                    >
+                      <CheckCircleOutlineIcon
+                        style={{ color: "white" }}
+                        fontSize="default"
+                      />
+                      &nbsp;&nbsp;&nbsp;Loại bỏ&nbsp;&nbsp;
+                    </Button>
+                  </ThemeProvider>
+                </Tooltip>
+              );
+            },
+            isFreeAction: true,
+          },
+          {
+            icon: () => {
+              return (
+                <Tooltip
+                  title="Chọn tất cả"
+                  aria-label="Chọn tất cả"
+                  placement="top"
+                >
+                  <Checkbox
+                    checked={selectedAll}
+                    onChange={(e) => {
+                      //alert('checkAll = ' + selectedAll);
+                      let tempS = e.target.checked;
+                      setSelectedAll(e.target.checked);
+
+                      if (tempS) count = classList.length;
+                      else count = 0;
+
+                      classList.map((value, index) => {
+                        value.selected = tempS;
+                      });
+                    }}
+                  />
+                  {/* <div>&nbsp;&nbsp;&nbsp;Chọn tất cả&nbsp;&nbsp;</div> */}
+                </Tooltip>
+              );
+            },
+            isFreeAction: true,
+          },
+        ]}
       />
     </Card>
   );
