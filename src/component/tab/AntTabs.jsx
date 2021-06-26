767d20cd809a812db611cac1acfc5bd2295b510e
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-06-26 22:16:39 +0700

    upgrade UI of scroll button

--- src/component/tab/AntTabs.jsx
+++ src/component/tab/AntTabs.jsx
@@ -1,21 +1,36 @@
 import { TabScrollButton } from "@material-ui/core";
-import { teal } from "@material-ui/core/colors";
+import { grey, teal } from "@material-ui/core/colors";
 import { withStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import React from "react";
 
 const AntTabScrollButton = withStyles((theme) => ({
   root: {
+    opacity: 1,
+    position: "absolute",
+    height: 36,
+    width: 36,
+    marginTop: 6,
+    borderRadius: "50%",
+    backgroundColor: grey[200],
     overflow: "hidden",
-    transition: "width 0.5s",
-    "&.Mui-disabled": {
-      width: 0,
+    "&.MuiTabs-scrollButtons": {
+      "&:hover": { backgroundColor: "#ffffff" },
+      "&:first-of-type": { zIndex: 1 },
+      "&:last-of-type": { right: 0 },
     },
+
+    // Another way to fix scroll button
+    // transition: "width 0.5s",
+    // "&.Mui-disabled": {
+    //   width: 0,
+    // },
   },
 }))(TabScrollButton);
 
 export const AntTabs = withStyles({
   root: {
+    position: "relative",
     borderBottom: "1px solid #e8e8e8",
   },
   indicator: {
