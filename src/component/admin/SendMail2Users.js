f6ab46a2c3ee58f7434f82e17e2944e0b81ca4a2
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-07-13 13:55:32 +0700

    remove editor and add form validation

--- src/component/admin/SendMail2Users.js
+++ src/component/admin/SendMail2Users.js
@@ -1,22 +1,19 @@
-import DateFnsUtils from "@date-io/date-fns";
 import {
+  Box,
   Card,
-  CardActions,
   CardContent,
   TextField,
   Typography,
 } from "@material-ui/core/";
-import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import { convertToRaw, EditorState } from "draft-js";
-import draftToHtml from "draftjs-to-html";
-import React, { useEffect, useState } from "react";
-import { Editor } from "react-draft-wysiwyg";
+import _ from "lodash";
+import React, { useState } from "react";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
-import { authPost } from "../../api";
+import { request } from "../../api";
+import PrimaryButton from "../button/PrimaryButton";
+import TertiaryButton from "../button/TertiaryButton";
 import AlertDialog from "../common/AlertDialog";
 
 const useStyles = makeStyles((theme) => ({
@@ -37,25 +34,11 @@ const useStyles = makeStyles((theme) => ({
 
 let reDirect = null;
 
-const editorStyle = {
-  toolbar: {
-    background: "#90caf9",
-  },
-  editor: {
-    border: "1px solid black",
-    minHeight: "300px",
-  },
-};
-
 function SendMail2Users() {
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.auth.token);
   const classes = useStyles();
-  const [mailTitle, setMailTitle] = useState(null);
-  const [mailContent, setMailContent] = useState(null);
-  const [categoryId, setCategoryId] = useState(null);
 
-  const [categoryIdList, setCategoryIdList] = useState([]);
+  //
+  const { register, errors, watch, handleSubmit } = useForm();
 
   const [alertMessage, setAlertMessage] = useState({
     title: "Vui lòng nhập đầy đủ thông tin cần thiết",
@@ -65,8 +48,6 @@ function SendMail2Users() {
   const [alertSeverity, setAlertSeverty] = useState("info");
   const [openAlert, setOpenAlert] = useState(false);
   const history = useHistory();
-
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
   const handleCloseAlert = () => {
     setOpenAlert(false);
@@ -78,90 +59,67 @@ function SendMail2Users() {
       history.push(reDirect);
     }
   };
-  const onChangeEditorState = (editorState) => {
-    setEditorState(editorState);
-  };
 
-  useEffect(() => {}, []);
-  async function handleSubmit() {
-    let content = draftToHtml(convertToRaw(editorState.getCurrentContent()));
-    //setProblemStatement(statement);
-    //console.log("handleSubmit problem statement = " + problemStatement);
-
-    let body = {
-      mailTitle: mailTitle,
-      mailContent: content,
-    };
-    //let body = {problemId,problemName,statement};
-    let contestProblem = await authPost(
-      dispatch,
-      token,
-      "/send-mail-to-all-users",
-      body
-    );
-    //console.log("return contest problem ", contestProblem);
-
-    //history.push("contestprogramming");
+  function onSubmit(data) {
+    request("post", "/send-mail-to-all-users", undefined, {}, data);
   }
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    <>
       <Card>
         <CardContent>
           <Typography variant="h5" component="h2">
             Gửi mail
           </Typography>
-          <form className={classes.root} noValidate autoComplete="off">
+          <form
+            className={classes.root}
+            onSubmit={handleSubmit(onSubmit)}
+            noValidate
+            autoComplete="off"
+          >
             <div>
               <TextField
                 autoFocus
-                required
-                id="title"
-                label="Tiêu đề"
-                placeholder="Tiêu đề"
-                value={mailTitle}
-                onChange={(event) => {
-                  setMailTitle(event.target.value);
-                }}
+                label="Tiêu đề*"
+                name="mailTitle"
+                // variant="outlined"
+                value={watch("mailTitle")}
+                size="small"
+                error={!!errors.mailTitle}
+                helperText={errors.mailTitle?.message}
+                inputRef={register({
+                  required: "Trường này được yêu cầu",
+                  validate: (name) => {
+                    if (_.isEmpty(name.trim()))
+                      return "Vui lòng chọn tiêu đề hợp lệ";
+                  },
+                })}
               />
             </div>
             <div>
               <TextField
-                required
-                id="mailContent"
                 label="Nội dung"
-                placeholder="Nội dung"
-                value={mailContent}
-                onChange={(event) => {
-                  setMailContent(event.target.value);
-                }}
-              />
-              <Editor
-                editorState={editorState}
-                handlePastedText={() => false}
-                onEditorStateChange={onChangeEditorState}
-                toolbarStyle={editorStyle.toolbar}
-                editorStyle={editorStyle.editor}
+                multiline
+                name="mailContent"
+                // variant="outlined"
+                value={watch("mailContent")}
+                size="small"
+                // error={!!errors.mailContent}
+                // helperText={errors.mailContent?.message}
+                inputRef={register}
               />
             </div>
+            <Box>
+              <TertiaryButton
+                style={{ marginLeft: "45px" }}
+                onClick={() => history.push("contestprogramming")}
+              >
+                Hủy
+              </TertiaryButton>
+              <PrimaryButton type="submit">Gửi</PrimaryButton>
+            </Box>
           </form>
         </CardContent>
-        <CardActions>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ marginLeft: "45px" }}
-            onClick={handleSubmit}
-          >
-            Gửi
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => history.push("contestprogramming")}
-          >
-            Hủy
-          </Button>
-        </CardActions>
       </Card>
 
       <AlertDialog
@@ -178,7 +136,7 @@ function SendMail2Users() {
           },
         ]}
       />
-    </MuiPickersUtilsProvider>
+    </>
   );
 }
 
