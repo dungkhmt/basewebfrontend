1fddf84d4e3b180f7069253cffe0c99e34d13480
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-07-14 13:34:12 +0700

    handle error when attribute to is null

--- src/layout/sidebar/v1/ListItemLink.jsx
+++ src/layout/sidebar/v1/ListItemLink.jsx
@@ -1,5 +1,6 @@
 import { ListItem } from "@material-ui/core";
-import PropTypes from "prop-types";
+import _ from "lodash";
+// import PropTypes from "prop-types";
 import React, { forwardRef, useMemo } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
@@ -12,15 +13,19 @@ export default function ListItemLink({ className, to, children, ...props }) {
     [to]
   );
 
-  return (
+  return _.isString(to) ? (
     <li>
       <ListItem className={className} component={renderLink} {...props}>
         {children}
       </ListItem>
     </li>
+  ) : (
+    <ListItem className={className} {...props}>
+      {children}
+    </ListItem>
   );
 }
 
-ListItemLink.propTypes = {
-  to: PropTypes.string.isRequired,
-};
+// ListItemLink.propTypes = {
+//   to: PropTypes.string.isRequired,
+// };
