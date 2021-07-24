b7f83d1674e4b0d7c8cb95688ced3852af41bd9a
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-07-24 18:40:45 +0700

    export function: getRandomIntInclusive

--- src/layout/notification/Notification.jsx
+++ src/layout/notification/Notification.jsx
@@ -90,7 +90,7 @@ const formatTime = (createdTime) => {
   }
 };
 
-function getRandomIntInclusive(min, max) {
+export function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
