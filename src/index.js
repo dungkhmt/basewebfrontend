3acb9f977c287f1cffcd1ed733bc48d300368f29
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-07-25 13:41:29 +0700

    add open street map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./wdyr"; // <--- first import
+// import "./wdyr"; // <--- first import
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
