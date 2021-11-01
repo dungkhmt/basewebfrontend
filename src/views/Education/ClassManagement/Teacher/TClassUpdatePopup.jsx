52b0e6f3ee7289ea790b5aadcb7c92a1ce202ad6
dungpq <dungkhmt@gmail.com>
2021-11-01 12:42:01 +0700

    upgrade class role

--- src/views/Education/ClassManagement/Teacher/TClassUpdatePopup.jsx
+++ src/views/Education/ClassManagement/Teacher/TClassUpdatePopup.jsx
@@ -3,13 +3,16 @@ import {
   DialogContent,
   DialogTitle,
   TextField,
+  MenuItem,
 } from "@material-ui/core";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
 import { request } from "../../../../api";
 
 export default function TClassUpdatePopup(props) {
   const { open, performUpdate, setOpen, classId } = props;
+  const [roles, setRoles] = useState([]);
+  const [selectedRole, setSelectedRole] = useState(null);
   function updateStatus(status) {
     request(
       "get",
@@ -18,11 +21,31 @@ export default function TClassUpdatePopup(props) {
       setOpen(false);
     });
   }
+  function getRoles() {
+    request("GET", "/edu/class/get-role-list-educlass-userlogin", (res) => {
+      setRoles(res.data);
+      console.log("getRoles res = ", res);
+    });
+  }
+  useEffect(() => {
+    getRoles();
+  }, []);
   return (
     <Dialog open={open}>
       <DialogTitle>Register information</DialogTitle>
       <DialogContent>
-        <TextField label="input"></TextField>
+        <TextField
+          label="input"
+          required
+          select
+          onChange={(e) => setSelectedRole(e.target.value)}
+        >
+          {roles.map((item) => (
+            <MenuItem key={item.roleId} value={item.roleId}>
+              {item.description}
+            </MenuItem>
+          ))}
+        </TextField>
         <Button onClick={() => updateStatus("HIDDEN")}>Hide</Button>
         <Button onClick={() => updateStatus("OPEN")}>Open</Button>
         <Button onClick={() => performUpdate()}>Save</Button>
