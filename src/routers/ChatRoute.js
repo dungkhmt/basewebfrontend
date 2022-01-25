c995d72d8f47643643b7981dabc49d31ddad6646
Minh Hoàng <52208309+mHoang99@users.noreply.github.com>
2022-01-25 15:38:41 +0700

    vmhoang: Chat Text Module

--- src/routers/ChatRoute.js
+++ src/routers/ChatRoute.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router";
-import ChatMessengerMain from "../component/chat/ChatMessengerMain";
+import ChatMessengerMain from "../component/chat/chatText/ChatMessengerMain";
 import ChatVoiceMain from "../component/chat/ChatVoiceMain/";
 import Room from "../component/chat/ChatVoiceMain/room";
 export default function ChatRoute() {
-  let { path } = useRouteMatch();
-  return (
-    <div>
-      <Switch>
-        <Route
-          component={ChatMessengerMain}
-          exact
-          path={`${path}/messenger/main`}
-        ></Route>
-        <Route
-          component={ChatVoiceMain}
-          exact
-          path={`${path}/voice/main`}
-        ></Route>
-        <Route
-          component={Room}
-          exact
-          path={`${path}/voice/main/:id`}
-        ></Route>
-      </Switch>
-    </div>
-  );
+    let { path } = useRouteMatch();
+    return (
+        <div>
+            <Switch>
+                <Route
+                    component={ChatMessengerMain}
+                    exact
+                    path={`${path}/messenger/main`}
+                ></Route>
+                <Route
+                    component={ChatVoiceMain}
+                    exact
+                    path={`${path}/voice/main`}
+                ></Route>
+                <Route
+                    component={Room}
+                    exact
+                    path={`${path}/voice/main/:id`}
+                ></Route>
+            </Switch>
+        </div>
+    );
 }
